c752e95ab3d577b22763bd3c280eaf93f1dbdabf
CARL <xiezhiweiisme@outlook.com>
2019-06-17 17:40:08 +0800

    change service

--- app/src/main/java/com/carl/applemusictimer/MusicService.java
+++ app/src/main/java/com/carl/applemusictimer/MusicService.java
@@ -1,8 +1,12 @@
 package com.carl.applemusictimer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * @author 创建人 ：xiezhiwei
@@ -15,9 +19,49 @@ import android.os.IBinder;
  */
 public class MusicService extends Service {
     private final String TAG = MusicService.class.getName();
-//    @androidx.annotation.Nullable
+    private AudioManager mAudioManager;
+    private boolean isPlaying = false;
+    private MyOnAudioFocusChangeListener audioFocusChangeListener;
+
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        // 获取系统音乐服务
+        mAudioManager = (AudioManager) getApplicationContext().getSystemService(
+                Context.AUDIO_SERVICE);
+        // 获取系统音乐服务状态
+        isPlaying=mAudioManager.isMusicActive();
+        audioFocusChangeListener = new MyOnAudioFocusChangeListener();
+        if(isPlaying) {//播放状态
+            int result = mAudioManager.requestAudioFocus(audioFocusChangeListener,
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+            {
+                Log.d(TAG, "requestAudioFocus successfully.");
+            }
+            else
+            {
+                Log.d(TAG, "requestAudioFocus failed.");
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public class MyOnAudioFocusChangeListener implements AudioManager.OnAudioFocusChangeListener {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            // TODO Auto-generated method stub
+        }
     }
 }
